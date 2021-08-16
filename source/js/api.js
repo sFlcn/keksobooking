67da185d284ebf1003f90b94d3beb8b8175d3d56
sFlcn <sfalcn@gmail.com>
2021-08-16 18:12:24 +0300

    Сборка для GitHub Pages #1

--- source/js/api.js
+++
@@ -1,9 +0,0 @@
-const fetchData = async (targetUrl, options) => {
-  const response = await fetch(targetUrl, options);
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Error(`${response.status} ${response.statusText}`);
-}
-
-export {fetchData};
